c287cb8461d6f513d12bd264a42e709cbb1fe341
jaugustodantas <jnetodf@gmail.com>
2020-11-14 21:34:16 -0300

    Teste inicial adicionado bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.ReadLine();
         }
     }
 }
